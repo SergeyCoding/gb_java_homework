842959f6c402ab05f56315b6bbfcc2363e7f7e11
SergeyXP <s.pusthotin@gmail.com>
2022-12-09 23:33:09 +0400

    homework02

--- Seminar01/homework01/src/main/java/org/homework/seminar01/task01/Task01.java
+++ Seminar01/homework01/src/main/java/org/homework/seminar01/task01/Task01.java
@@ -3,31 +3,39 @@ package org.homework.seminar01.task01;
 import java.util.Scanner;
 
 /**
- * Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+ * Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение
+ * чисел от 1 до n)
  */
 public class Task01 {
+
     public static void run() {
 
         System.out.println("\nTask01");
 
         System.out.print("Номер: ");
-        var numStr = new Scanner(System.in).nextLine();
 
-        int num = Integer.parseInt(numStr);
+        try (Scanner scanner = new Scanner(System.in)) {
+            final var numStr = scanner.nextLine();
 
-        if (num < 0) {
-            System.out.println("Введите корректные данные");
-            return;
+            int num = Integer.parseInt(numStr);
+
+            if (num < 0) {
+                System.out.println("Введите корректные данные");
+                return;
+            }
+
+            int sum = 0;
+            int factorial = 1;
+            for (int i = 1; i <= num; i++) {
+                sum += i;
+                factorial *= i;
+            }
+
+            System.out.println("Треугольное число: " + sum);
+            System.out.println("Факториал: " + factorial);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            throw e;
         }
-
-        int sum = 0;
-        int factorial = 1;
-        for (int i = 1; i <= num; i++) {
-            sum += i;
-            factorial *= i;
-        }
-
-        System.out.println("Треугольное число: " + sum);
-        System.out.println("Факториал: " + factorial);
     }
 }
--- Seminar01/homework01/src/main/java/org/homework/seminar01/task03/SimpleCalc.java
+++ Seminar01/homework01/src/main/java/org/homework/seminar01/task03/SimpleCalc.java
@@ -2,7 +2,6 @@ package org.homework.seminar01.task03;
 
 import java.util.Scanner;
 
-
 /**
  * Реализовать простой калькулятор
  */
@@ -10,25 +9,27 @@ public class SimpleCalc {
     public static void run() {
         System.out.println("\nTask03");
 
-        var sc = new Scanner(System.in);
+        try (var sc = new Scanner(System.in)) {
+            System.out.println("Вводите сначала два числа, потом операцию (+, -, *, /)");
+            System.out.print("Первое число:");
+            double num1 = Double.parseDouble(sc.nextLine());
 
-        System.out.println("Вводите сначала два числа, потом операцию (+, -, *, /)");
-        System.out.print("Первое число:");
-        double num1 = Double.parseDouble(sc.nextLine());
+            System.out.print("Второе число:");
+            double num2 = Double.parseDouble(sc.nextLine());
 
-        System.out.print("Второе число:");
-        double num2 = Double.parseDouble(sc.nextLine());
+            System.out.print("Операция:");
+            char op = sc.nextLine().charAt(0);
 
-        System.out.print("Операция:");
-        char op = sc.nextLine().charAt(0);
+            var res = operation(num1, num2, op);
+            if (Double.isNaN(res)) {
+                System.out.println("Ошибка!!!");
+                return;
+            }
 
-        var res = operation(num1, num2, op);
-        if (Double.isNaN(res)) {
-            System.out.println("Ошибка!!!");
-            return;
+            System.out.printf("%f %c %f = %f", num1, op, num2, res);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
         }
-
-        System.out.printf("%f %c %f = %f", num1, op, num2, res);
     }
 
     static double operation(double n1, double n2, char op) {
