14cf00b066c9fd8b9cf7380bcd945aa12783cc91
SergeyXP <s.pusthotin@gmail.com>
2022-12-13 23:10:48 +0400

    homework03

---
+++ Seminar03/src/main/java/org/homework/seminar03/Main.java
@@ -0,0 +1,14 @@
+package org.homework.seminar03;
+
+/**
+ * Реализовать алгоритм сортировки слиянием
+ *
+ * Пусть дан произвольный список целых чисел, удалить из него четные числа
+ *
+ * Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Seminar03/src/main/java/org/homework/seminar03/task01/QuickSort.java
@@ -0,0 +1,7 @@
+package org.homework.seminar03.task01;
+
+/**
+ * Реализовать алгоритм сортировки слиянием
+ */
+public class QuickSort {
+}
