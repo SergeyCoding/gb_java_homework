e8d0f2fd772904f7e90901540e84e309903e48bb
SergeyXP <s.pusthotin@gmail.com>
2022-12-17 17:16:05 +0400

    task02

--- Seminar04/src/main/java/org/homework/seminar04/task02/QueueLinkedList.java
+++ Seminar04/src/main/java/org/homework/seminar04/task02/QueueLinkedList.java
@@ -1,6 +1,34 @@
 package org.homework.seminar04.task02;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * 2. Реализуйте очередь с помощью LinkedList со следующими методами:enqueue() - помещает
+ * элемент в конец очереди, dequeue() - возвращает первый элемент из очереди и удаляет его,
+ * first() - возвращает первый элемент из очереди, не удаляя.
+ */
 public class QueueLinkedList {
-    public static void run() {
+
+    private final LinkedList<Integer> list = new LinkedList<>();
+
+    public QueueLinkedList() {
+    }
+
+    public void enqueue(int e) {
+        list.addLast(e);
+    }
+
+    public int dequeue() {
+        return list.removeFirst();
+    }
+
+    public Integer first() {
+        return list.getFirst();
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(list.toArray());
     }
 }
--- Seminar04/src/main/java/org/homework/seminar04/task02/QueueLinkedListManager.java
+++ Seminar04/src/main/java/org/homework/seminar04/task02/QueueLinkedListManager.java
@@ -9,6 +9,19 @@ public class QueueLinkedListManager {
     public static void run() {
         System.out.println("\nСеминар 4.");
         System.out.println("Задача 2.");
+
+        var queueLinkedList = new QueueLinkedList();
+
+        queueLinkedList.enqueue(1);
+        System.out.printf("enqueue(1)\t%s\n", queueLinkedList);
+        queueLinkedList.enqueue(20);
+        System.out.printf("enqueue(20)\t%s\n", queueLinkedList);
+        var a = queueLinkedList.dequeue();
+        System.out.printf("dequeue()\t%s\t->%s\n", queueLinkedList, a);
+        queueLinkedList.enqueue(30);
+        System.out.printf("enqueue(30)\t%s\n", queueLinkedList);
+
+        System.out.printf("first()\t%s\t->%s\n", queueLinkedList, queueLinkedList.first());
     }
 }
 
