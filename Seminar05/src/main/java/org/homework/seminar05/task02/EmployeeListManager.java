4a1b3c181ac9522b8a58ad3b68e758cb7620a272
SergeyXP <s.pusthotin@gmail.com>
2022-12-21 15:25:52 +0400

    homework05

--- Seminar05/src/main/java/org/homework/seminar05/task02/EmployeeListManager.java
+++ Seminar05/src/main/java/org/homework/seminar05/task02/EmployeeListManager.java
@@ -1,9 +1,14 @@
 package org.homework.seminar05.task02;
 
-import org.homework.tools.ConsoleHelper;
-import org.homework.tools.ListGenerator;
-
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 2. Пусть дан список сотрудников:
@@ -13,15 +18,33 @@ import java.util.Arrays;
 public class EmployeeListManager {
     public static void run() {
         System.out.println("\nСеминар 5");
-        System.out.println("Задача 2");
+        System.out.println("Задача 2\n");
 
-        int n = ConsoleHelper.getNaturalInteger("Введите размер массива: ");
+        var p = System.getProperty("user.dir");
+
+        Path filePath = Paths.get(p, "data.txt");
+        Charset charset = StandardCharsets.UTF_8;
+
+        try {
+
+            List<String> strings = Files.lines(filePath, charset).toList();
+            List<Person> personList = new LinkedList<>();
+
+            for (var s : strings) {
+                var zz = s.trim().split("\\s");
+                personList.add(new Person(zz[0], zz[1]));
+            }
+
+            EmployeeList employeeList = new EmployeeList(personList);
+
+            employeeList.getPopularNames().entrySet().stream()
+                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                    .forEach(System.out::println);
+
+        } catch (IOException ex) {
+            System.out.format("I/O error: %s%n", ex);
+        }
 
 
-        var linkedList = ListGenerator.generateLinkedList(n);
-        System.out.println(Arrays.toString(linkedList.toArray()));
-
-//        var revertLinkedList = new RevertLinkedList(linkedList);
-//        System.out.println(Arrays.toString(revertLinkedList.revert().toArray()));
     }
 }
