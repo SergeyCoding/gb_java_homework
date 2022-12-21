d9dc9e88e4b90f1c28f91a2f05f55263395a6cdb
SergeyXP <s.pusthotin@gmail.com>
2022-12-21 14:01:18 +0400

    task01/Phonebook

--- Seminar05/src/main/java/org/homework/seminar05/task01/PhonebookManager.java
+++ Seminar05/src/main/java/org/homework/seminar05/task01/PhonebookManager.java
@@ -1,45 +1,40 @@
 package org.homework.seminar05.task01;
 
-import org.homework.tools.ConsoleHelper;
-import org.homework.tools.ListGenerator;
-
-import java.util.Arrays;
-
 /**
  * 1. Реализуйте структуру телефонной книги с помощью HashMap, учитывая,
  * что 1 человек может иметь несколько телефонов.
- * <p>
- * Иван Иванов
- * Светлана Петрова
- * Кристина Белова
- * Анна Мусина
- * Анна Крутова
- * Иван Юрин
- * Петр Лыков
- * Павел Чернов
- * Петр Чернышов
- * Мария Федорова
- * Марина Светлова
- * Мария Савина
- * Мария Рыкова
- * Марина Лугова
- * Анна Владимирова
- * Иван Мечников
- * Петр Петин
- * Иван Ежов
  */
 public class PhonebookManager {
     public static void run() {
         System.out.println("\nСеминар 5");
         System.out.println("Задача 1");
 
-        int n = ConsoleHelper.getNaturalInteger("Введите размер массива: ");
+        Phonebook phonebook = new Phonebook();
 
+        phonebook.add("a1", "+79003005081");
+        phonebook.add("a2", "+79003005082");
+        phonebook.add("a3", "+79003005083");
+        phonebook.add("a1", "+79003005084");
+        phonebook.add("a1", "+79003005085");
+        phonebook.add("a5", "+79003005086");
+        phonebook.add("a5", "+79003005087");
 
-        var linkedList = ListGenerator.generateLinkedList(n);
-        System.out.println(Arrays.toString(linkedList.toArray()));
+        System.out.println("Список контактов");
+        System.out.println(phonebook.getNames());
 
-//        var revertLinkedList = new RevertLinkedList(linkedList);
-//        System.out.println(Arrays.toString(revertLinkedList.revert().toArray()));
+        System.out.println("\nСписок телефонов контакта 'a1'");
+        System.out.println(phonebook.getPhones("a1"));
+
+        System.out.println("\nСписок телефонов контакта 'a2'");
+        System.out.println(phonebook.getPhones("a2"));
+
+        System.out.println("\nУдалили телефон у контакта 'a1'");
+        phonebook.remove("a1", "+79003005081");
+        System.out.println(phonebook.getPhones("a1"));
+
+        System.out.println("\nУдалили все телефоны у контакта 'a5'. Исчез из списка контактов");
+        phonebook.remove("a5", "+79003005086");
+        phonebook.remove("a5", "+79003005087");
+        System.out.println(phonebook.getNames());
     }
 }
