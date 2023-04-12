17081278222cf31e8821b0a4427332363be05544
SergeyXP <s.pusthotin@gmail.com>
2023-04-12 09:15:07 +0400

    seminar03 done

--- 11-algo-homework/seminar03/src/main/java/org/gb/algo/Main.java
+++ 11-algo-homework/seminar03/src/main/java/org/gb/algo/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println("\nTask03");
         task03();
 
-        System.out.println("#");
+        System.out.println("\n#");
     }
 
     private static void task01() {
