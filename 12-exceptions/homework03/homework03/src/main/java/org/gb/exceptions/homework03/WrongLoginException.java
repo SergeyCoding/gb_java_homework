4759c3e094f23ab186f900a8ed6c7797d5541108
SergeyXP <s.pusthotin@gmail.com>
2023-04-26 11:18:29 +0400

    12-exceptions/homework03

---
+++ 12-exceptions/homework03/homework03/src/main/java/org/gb/exceptions/homework03/WrongLoginException.java
@@ -0,0 +1,13 @@
+package org.gb.exceptions.homework03;
+
+public class WrongLoginException extends Exception {
+
+	public WrongLoginException() {
+		super();
+	}
+
+	public WrongLoginException(String message) {
+		super(message);
+	}
+
+}
---
+++ 12-exceptions/homework03/homework03/src/main/java/org/gb/exceptions/homework03/WrongPasswordException.java
@@ -0,0 +1,13 @@
+package org.gb.exceptions.homework03;
+
+public class WrongPasswordException extends Exception {
+
+	public WrongPasswordException() {
+		super();
+	}
+
+	public WrongPasswordException(String message) {
+		super(message);
+	}
+
+}
